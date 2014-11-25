9bb2913a7df919e9e81976e16825aa5d53746aa8
ZhuoranLyu <zl953@nyu.edu>
2014-11-25 11:49:34 -0500

    plot figures using js library

--- FinalProject/DisplayAndInteractive/views.py
+++ FinalProject/DisplayAndInteractive/views.py
@@ -13,19 +13,22 @@ import datetime
 import matplotlib.pyplot as plt#, mpld3
 
 def home(request):
+
+	'''
+	Get user input from html and use webDataReader to get stock information,
+	then dumps the stat and figures to html using json.dumps()
+	'''
+
 	context = {}
 	context['requestMethod'] = request.META['REQUEST_METHOD']
 
 	if request.method == 'GET' :
 
 		if request.GET.__contains__('hidden') :
-			#if 'name' in context and 'end_date' in context and 'start_date' in context:
 			context['name'] = request.GET['name']
 			context['start_date'] = request.GET['start_date']
 			context['end_date'] = request.GET['end_date']
 	
-	print context
-				
 	requestContext = RequestContext(request, context)
 
 	templateIndex = loader.get_template('index.html')
@@ -38,30 +41,29 @@ def home(request):
 
 	response.write(renderedTemplate)
 	
-	if 'name' in context:# and 'end_date' in context and 'start_date' in context:
-		company = context['name'].encode('ascii','ignore')
-		start_date = context['start_date'].encode('ascii','ignore')
-		end_date = context['end_date'].encode('ascii','ignore')
-		a = request.GET.get('name')
-		start_date = start_date.split('-')
-		end_date = end_date.split('-')
+	if 'name' in context:  
+ 
+		# if there is an input from user, get that input
+
+		company_name = context['name'].encode('ascii','ignore')
+		start_date = context['start_date'].encode('ascii','ignore').split('-')
+		end_date = context['end_date'].encode('ascii','ignore').split('-')
+		stat_type = 'Close'
+
+		# change the date to datetime in python
 		
-		start = datetime.datetime(int(start_date[0]),int(start_date[1]),int(start_date[2]))
-		end = datetime.datetime(int(end_date[0]),int(end_date[1]),int(end_date[2]))
+		start_date = datetime.datetime(int(start_date[0]),int(start_date[1]),int(start_date[2]))
+		end_date = datetime.datetime(int(end_date[0]),int(end_date[1]),int(end_date[2]))
 		
-		f = web.DataReader(company,'yahoo',start,end)
-		a = f['Close']
-		b = a.index.tolist()
-		array = []
-		for i in range(b.__len__()):
-			c = b[i]
-			s = str(c)[:10]
-			d = a.tolist()
-			e = round(d[i],2)
-			array.append(e)
+		#using DataReader to get info from yahoo and put prices to a list
+		
+		df = web.DataReader(company_name,'yahoo',start_date,end_date)
+		price_list = [round(x,2) for x in df[price_type].tolist()]
+
 		#print f
         #fig = figure(1)
         #plot([3,1,2,4,1])
         #js_data = json.dumps(mpld3.fig_to_dict(fig))
-		return render_to_response('index.html',{'array':json.dumps(array), 'start_date':json.dumps(start_date), 'end_date':json.dumps(end_date)})
+
+		return render_to_response('index.html',{'array':json.dumps(price_list), 'start_date':json.dumps(start_date), 'end_date':json.dumps(end_date)})
 	return render_to_response('index.html',{})
